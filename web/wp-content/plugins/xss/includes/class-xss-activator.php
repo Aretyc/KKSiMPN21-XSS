3ee46877a829b236a4f0b92f5e759d18fda26029
aretyc <szymon1621997@gmail.com>
2021-09-02 15:45:44 +0200

    add init db table

--- web/wp-content/plugins/xss/includes/class-xss-activator.php
+++ web/wp-content/plugins/xss/includes/class-xss-activator.php
@@ -30,7 +30,17 @@ class Xss_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		$sql = "CREATE TABLE `wp_xss_message` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`name` text NOT NULL,
+			`message` text NOT NULL,
+			PRIMARY KEY (`id`)
+		  )  $charset_collate;";
+	
+		dbDelta( $sql );
 	}
 
 }
